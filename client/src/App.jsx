dbd30db37af6d997f0f0217897df5204a0527924
Fabrice Yong <fabricenjua@gmail.com>
2024-01-19 14:29:04 +0100

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,66 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+// Pages
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
+
+// components
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/signin',
+        element: <SignIn />
+      },
+      {
+        path: '/signup',
+        element: <SignUp />,
+      },
+      {
+        path: '/about',
+        element: <About />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+      },
+    {
+      path: '/projects',
+      element: <Projects />,
+    },
+    ]
+  }
+])
 
 function App() {
 
   return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   )
 }
 
