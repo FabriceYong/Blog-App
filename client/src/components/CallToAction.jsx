9a3876e329d1205799d9ed5027befc624cf7aae8
Fabrice Yong <fabricenjua@gmail.com>
2024-02-24 20:11:39 +0100

    add call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,24 @@
+import { Button } from 'flowbite-react'
+import React from 'react'
+
+const CallToAction = () => {
+  return (
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl gap-6'>
+      <div className="flex-1 justify-center flex flex-col gap-3">
+        <h2 className='text-3xl'>Want to learn more about JavaScript?</h2>
+        <p className='text-gray-500'>Checkout these resources with 100 JavaScript Projects</p>
+        <Button gradientDuoTone={'purpleToPink'}>Learn More</Button>
+      </div>
+      <div className="p-7 flex-1">
+        <img
+          src={
+            'https://cyberhoot.com/wp-content/uploads/2020/07/Free-Courses-to-learn-JavaScript.jpg'
+          }
+          alt="image"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default CallToAction
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import axiosRequest from '../utils/axiosRequest'
 import { Button, Spinner } from 'flowbite-react'
+import CallToAction from '../components/CallToAction'
 
 const PostPage = () => {
     const { postSlug } = useParams()
@@ -29,7 +30,7 @@ const PostPage = () => {
     }, [postSlug])
   return (
     <div className="dark:text-gray-200">
-      {loading && (
+      {loading ? (
         <div className="flex items-center gap-3 my-4 justify-center min-h-screen">
           <Spinner
             color="purple"
@@ -37,19 +38,44 @@ const PostPage = () => {
             size="xl"
           />
         </div>
-      )}
-      <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
-        <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>{post?.title}</h1>
-        <Link to={`/search?category=${post?.category}`} className='self-center my-5'>
-            <Button color='failure' outline pill size={'xs'}>{post?.category}</Button>
-        </Link>
-        <img src={post?.image} alt={post?.title} className='mt-5 p-3 max-h-[600px] w-full object-cover' />
-        <div className="flex justify-between p-3 border-b border-slate-300 mx-auto w-full mx-w-2xl text-xs font-medium">
+      ) : error ? (
+        <p className="text-center text-gray-600 text-3xl font-medium">
+          Sorry? There was an error fetching the post, refresh or try again
+          later
+        </p>
+      ) : (
+        <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+          <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+            {post?.title}
+          </h1>
+          <Link
+            to={`/search?category=${post?.category}`}
+            className="self-center my-5"
+          >
+            <Button color="failure" outline pill size={'xs'}>
+              {post?.category}
+            </Button>
+          </Link>
+          <img
+            src={post?.image}
+            alt={post?.title}
+            className="mt-5 p-3 max-h-[600px] w-full object-cover"
+          />
+          <div className="flex justify-between p-3 border-b border-slate-300 mx-auto w-full mx-w-2xl text-xs font-medium">
             <span>{new Date(post?.createdAt).toLocaleDateString()}</span>
-            <span className='italic'>{(post?.content.length / 1000).toFixed(0)}mins read</span>
-        </div>
-        <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post?.content}}></div>
-      </main>
+            <span className="italic">
+              {(post?.content.length / 1000).toFixed(0)}mins read
+            </span>
+          </div>
+          <div
+            className="p-3 max-w-2xl mx-auto w-full post-content"
+            dangerouslySetInnerHTML={{ __html: post?.content }}
+          ></div>
+
+            <div className="max-w-4xl mx-auto w-full"><CallToAction /></div>
+          
+        </main>
+      )}
     </div>
   )
 }
